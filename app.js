e003b78f1a51e916c9195f2205950d5a85c2df29
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2024-03-22 09:56:44 +0600

    cart api added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5051;
@@ -57,6 +57,42 @@ const mdbClient = new MongoClient(process.env.MONGODB_URI, {
         },
         data,
       });
+    });
+
+    app.post("/products", async (req, res) => {
+      if (req.query.ids) {
+        const page = +req.query.page || 1;
+        const limit = +req.query.limit || 10;
+        const skip = (page - 1) * limit;
+        const ids = req.body.map((id) => new ObjectId(id));
+
+        const query = { _id: { $in: ids } };
+        const cursor = products.find(query).skip(skip).limit(limit);
+        const data = await cursor.toArray();
+        const total = await products.countDocuments(query);
+
+        res.send({
+          success: true,
+          statusCode: 200,
+          message: "Fetched successful!",
+          meta: {
+            page,
+            limit,
+            skip,
+            total,
+          },
+          data,
+        });
+      } else {
+        const data = await products.insertOne(req.body);
+
+        res.send({
+          success: true,
+          statusCode: 200,
+          message: "Created successfully!",
+          data,
+        });
+      }
     });
 
     mdbClient
